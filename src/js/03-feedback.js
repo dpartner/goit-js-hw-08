621bd2a76e324f2db245cb58594abbcd58ce32b9
dpartner <d.partner.ua@gmail.com>
2022-03-28 18:46:19 +0300

    3 task completed

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,38 @@
+import { throttle } from 'lodash';
 
+const form = document.querySelector('.feedback-form');
+
+form.addEventListener('input', throttle(onInput, 500));
+form.addEventListener('submit', onSubmit);
+
+let currentValue = onCurrentValueOfInput();
+
+function onInput(e) {
+  const inputName = e.target.name;
+  const inputValue = e.target.value;
+
+  if (inputName === 'email') {
+    currentValue.email = inputValue;
+  } else if (inputName === 'message') {
+    currentValue.message = inputValue;
+  }
+
+  localStorage.setItem('feedback-form-state', JSON.stringify(currentValue));
+}
+
+function onCurrentValueOfInput() {
+  const currentValue = JSON.parse(localStorage.getItem('feedback-form-state'));
+
+  if (currentValue) {
+    form.email.value = currentValue.email;
+    form.message.value = currentValue.message;
+    return currentValue;
+  }
+  return {};
+}
+
+function onSubmit(e) {
+  e.preventDefault();
+  e.currentTarget.reset();
+  console.log('Ваш отзыв отправлен', currentValue);
+}
